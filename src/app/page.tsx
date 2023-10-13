f2b043fd3433c2fc88c6acb7b9c61a009684f862
seyiram <yoggi.jerry@gmail.com>
2023-10-12 22:48:36 -0300

    Add initial setup - Add clerk authentication - Add landing page copy and buttons - Add sign-up and sig-in pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,42 @@
+import { Button } from "@/components/ui/button";
+import { UserButton, auth } from "@clerk/nextjs";
+import Link from "next/link";
+import { LogIn } from "lucide-react";
 
-
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth();
+  console.log("userId: " + userId);
+  const isAuth = !!userId;
   return (
-    <>
-      <h1>JusticeIQ</h1>
-    </>
+    <div className="w-screen min-h-screen bg-gradient-to-r from-green-300 to-purple-400">
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            <h1 className="mr-3 text-5xl font-semibold text-white">
+              Chat with any PDF
+            </h1>
+            <UserButton afterSignOutUrl="/" />
+          </div>
+          <div className="flex mt-2">
+            {isAuth && <Button>Go to Chats</Button>}
+          </div>
+          <p className="max-w-xl text-lg text-gray-800 mt-3">
+            Join millions of law students, researchers and professionals to
+            instantly answer questions and understand research using AI
+          </p>
+          <div className="w-full mt-4">
+            {isAuth ? (
+              <h1>fileupload</h1>
+            ) : (
+              <Link href="/sign-in">
+                <Button>
+                  Login to get started <LogIn className="w-4 h-4 ml-2" />
+                </Button>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
